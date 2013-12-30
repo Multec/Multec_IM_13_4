b31d1049138d3e11b145623cb6d4da00f5ddb441
wesleyvandevoorde <vandevoordewesley@gmail.com>
2013-12-30 12:57:19 +0100

    TEST COMMIT WESLEY

---
+++ src/infopanel/scene4.java
@@ -0,0 +1,301 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import org.mt4j.MTApplication;
+import org.mt4j.components.MTComponent;
+import org.mt4j.components.TransformSpace;
+import org.mt4j.components.visibleComponents.font.FontManager;
+import org.mt4j.components.visibleComponents.font.IFont;
+import org.mt4j.components.visibleComponents.shapes.MTEllipse;
+import org.mt4j.components.visibleComponents.shapes.MTRectangle;
+import org.mt4j.components.visibleComponents.shapes.MTRectangle.PositionAnchor;
+import org.mt4j.components.visibleComponents.widgets.MTBackgroundImage;
+import org.mt4j.components.visibleComponents.widgets.MTTextArea;
+import org.mt4j.components.visibleComponents.widgets.MTTextField;
+import org.mt4j.components.visibleComponents.widgets.buttons.MTImageButton;
+import org.mt4j.input.gestureAction.DefaultArcballAction;
+import org.mt4j.input.gestureAction.DefaultDragAction;
+import org.mt4j.input.gestureAction.DefaultLassoAction;
+import org.mt4j.input.gestureAction.DefaultPanAction;
+import org.mt4j.input.gestureAction.DefaultRotateAction;
+import org.mt4j.input.gestureAction.DefaultScaleAction;
+import org.mt4j.input.gestureAction.DefaultZoomAction;
+import org.mt4j.input.gestureAction.InertiaDragAction;
+import org.mt4j.input.gestureAction.TapAndHoldVisualizer;
+import org.mt4j.input.inputProcessors.IGestureEventListener;
+import org.mt4j.input.inputProcessors.MTGestureEvent;
+import org.mt4j.input.inputProcessors.componentProcessors.arcballProcessor.ArcballProcessor;
+import org.mt4j.input.inputProcessors.componentProcessors.dragProcessor.DragProcessor;
+import org.mt4j.input.inputProcessors.componentProcessors.lassoProcessor.LassoProcessor;
+import org.mt4j.input.inputProcessors.componentProcessors.panProcessor.PanProcessorTwoFingers;
+import org.mt4j.input.inputProcessors.componentProcessors.rotateProcessor.RotateProcessor;
+import org.mt4j.input.inputProcessors.componentProcessors.scaleProcessor.ScaleProcessor;
+import org.mt4j.input.inputProcessors.componentProcessors.tapAndHoldProcessor.TapAndHoldEvent;
+import org.mt4j.input.inputProcessors.componentProcessors.tapAndHoldProcessor.TapAndHoldProcessor;
+import org.mt4j.input.inputProcessors.componentProcessors.tapProcessor.TapEvent;
+import org.mt4j.input.inputProcessors.componentProcessors.tapProcessor.TapProcessor;
+import org.mt4j.input.inputProcessors.componentProcessors.zoomProcessor.ZoomProcessor;
+import org.mt4j.input.inputProcessors.globalProcessors.CursorTracer;
+import org.mt4j.sceneManagement.AbstractScene;
+import org.mt4j.sceneManagement.Iscene;
+import org.mt4j.sceneManagement.transition.BlendTransition;
+import org.mt4j.sceneManagement.transition.FadeTransition;
+import org.mt4j.sceneManagement.transition.SlideTransition;
+import org.mt4j.util.MT4jSettings;
+import org.mt4j.util.MTColor;
+import org.mt4j.util.math.Vector3D;
+import org.mt4j.util.opengl.GLFBO;
+
+import processing.core.PImage;
+		
+	
+public class scene4 extends AbstractScene {
+		private MTApplication app;
+		private Iscene scene1;
+		private Iscene scene2;
+		private Iscene scene3;
+		private Iscene scene5;
+		
+		public scene4(final MTApplication mtApplication, String name) {
+			super(mtApplication, name);
+			this.app = mtApplication;
+			this.registerGlobalInputProcessor(new CursorTracer(app, this));
+			MTBackgroundImage background = new MTBackgroundImage(mtApplication, mtApplication.loadImage("project.jpg") , true);
+			this.getCanvas().addChild(background);
+			
+			MTColor white = new MTColor(255,255,255);
+			MTColor black = new MTColor(0,0,0);
+			MTColor kleurbol1 = new MTColor(255, 51, 51, 125);
+			MTColor kleurbol2 = new MTColor(60, 146, 48, 125);
+			MTColor kleurbol3 = new MTColor(0, 102, 204, 125);
+			MTColor kleurbol4 = new MTColor(191, 21, 165, 125);
+			MTColor kleurbol5 = new MTColor(204, 153, 0, 125);
+			
+			MTColor textAreaColor = new MTColor(50,100,150,0);
+			
+			IFont font = FontManager.getInstance().createFont(app, "HYPE.ttf", 40, white, white);
+			IFont inhoudfont = FontManager.getInstance().createFont(app, "HYPE.ttf", 17, black, black);
+
+			//Tap gesture
+			MTEllipse circle = new MTEllipse(app, new Vector3D((mtApplication.width/5)*0 + 35, 50), 30, 30);
+			circle.setFillColor(kleurbol1);
+			circle.setNoStroke(true);
+			this.clearAllGestures(circle);
+			getCanvas().addChild(circle);
+			final MTTextArea tap1 = new MTTextArea(mtApplication, font);
+			tap1.setFillColor(textAreaColor);
+			tap1.setStrokeColor(textAreaColor);
+			tap1.setText("DEVELOPMENT");
+			this.clearAllGestures(tap1);
+			tap1.registerInputProcessor(new TapProcessor(app));
+			tap1.addGestureListener(TapProcessor.class, new IGestureEventListener() {
+				public boolean processGestureEvent(MTGestureEvent ge) {					
+					TapEvent te = (TapEvent)ge;
+					switch (te.getId()) {
+					case MTGestureEvent.GESTURE_DETECTED:
+						MTColor white = new MTColor(10,100,50);
+						break;
+					case MTGestureEvent.GESTURE_UPDATED:
+						break;
+					case MTGestureEvent.GESTURE_ENDED:
+						if (te.isTapped()){
+							app.pushScene();
+							if (scene1 == null){
+								scene1 = new scene1(app, "Scene 1");
+								//Add the scene to the mt application
+								app.addScene(scene1);
+							}
+							//Do the scene change
+							app.changeScene(scene1);
+							break;
+							}
+					default: break;
+				}
+					return false;			
+			}
+			});
+			getCanvas().addChild(tap1);
+			tap1.setAnchor(PositionAnchor.UPPER_LEFT);
+			tap1.setPositionGlobal(new Vector3D((mtApplication.width/5)*0,30,0));
+			
+			MTEllipse circle2 = new MTEllipse(app, new Vector3D((mtApplication.width/5)*1 + 35, 50), 30, 30);
+			circle2.setFillColor(kleurbol2);
+			circle2.setNoStroke(true);
+			getCanvas().addChild(circle2);
+			final MTTextArea tap2 = new MTTextArea(mtApplication, font);
+			tap2.setFillColor(textAreaColor);
+			tap2.setStrokeColor(textAreaColor);
+			tap2.setText("DESIGN");
+			this.clearAllGestures(tap2);
+			tap2.registerInputProcessor(new TapProcessor(app));
+			tap2.addGestureListener(TapProcessor.class, new IGestureEventListener() {
+				public boolean processGestureEvent(MTGestureEvent ge) {					
+					TapEvent te = (TapEvent)ge;
+					switch (te.getId()) {
+					case MTGestureEvent.GESTURE_DETECTED:
+						MTColor white = new MTColor(10,100,50);
+						break;
+					case MTGestureEvent.GESTURE_UPDATED:
+						break;
+					case MTGestureEvent.GESTURE_ENDED:
+						if (te.isTapped()){
+							app.pushScene();
+							if (scene2 == null){
+								scene2 = new scene2(app, "Scene 2");
+								//Add the scene to the mt application
+								app.addScene(scene2);
+							}
+							//Do the scene change
+							app.changeScene(scene2);
+							break;
+							}
+					default: break;
+				}
+					return false;
+					
+			}
+			});
+			getCanvas().addChild(tap2);
+			tap2.setAnchor(PositionAnchor.UPPER_LEFT);
+			tap2.setPositionGlobal(new Vector3D((mtApplication.width/5)*1,30,0));
+			
+			MTEllipse circle3 = new MTEllipse(app, new Vector3D((mtApplication.width/5)*2 + 35, 50), 30, 30);
+			circle3.setFillColor(kleurbol3);
+			circle3.setNoStroke(true);
+			getCanvas().addChild(circle3);
+			final MTTextArea tap3 = new MTTextArea(mtApplication, font);
+			tap3.setFillColor(textAreaColor);
+			tap3.setStrokeColor(textAreaColor);
+			tap3.setText("TECHNOLOGY");
+			this.clearAllGestures(tap3);
+			tap3.registerInputProcessor(new TapProcessor(app));
+			tap3.addGestureListener(TapProcessor.class, new IGestureEventListener() {
+				public boolean processGestureEvent(MTGestureEvent ge) {					
+					TapEvent te = (TapEvent)ge;
+					switch (te.getId()) {
+					case MTGestureEvent.GESTURE_DETECTED:
+						MTColor white = new MTColor(10,100,50);
+						break;
+					case MTGestureEvent.GESTURE_UPDATED:
+						break;
+					case MTGestureEvent.GESTURE_ENDED:
+						if (te.isTapped()){
+							app.pushScene();
+							if (scene3 == null){
+								scene3 = new scene3(app, "Scene 3");
+								//Add the scene to the mt application
+								app.addScene(scene3);
+							}
+							//Do the scene change
+							app.changeScene(scene3);
+							break;
+							}
+					default: break;
+				}
+					return false;			
+			}
+			});
+			getCanvas().addChild(tap3);
+			tap3.setAnchor(PositionAnchor.UPPER_LEFT);
+			tap3.setPositionGlobal(new Vector3D((mtApplication.width/5)*2,30,0));
+			
+			MTEllipse circle4 = new MTEllipse(app, new Vector3D((mtApplication.width/5)*3 + 35, 50), 30, 30);
+			circle4.setFillColor(kleurbol4);
+			circle4.setNoStroke(true);
+			getCanvas().addChild(circle4);
+			final MTTextArea tap4 = new MTTextArea(mtApplication, font);
+			tap4.setFillColor(textAreaColor);
+			tap4.setStrokeColor(textAreaColor);
+			tap4.setText("INTEGRATION PROJECT");
+			this.clearAllGestures(tap4);
+			tap4.registerInputProcessor(new TapProcessor(app));
+			getCanvas().addChild(tap4);
+			tap4.setAnchor(PositionAnchor.UPPER_LEFT);
+			tap4.setPositionGlobal(new Vector3D((mtApplication.width/5)*3,30,0));
+			
+			MTEllipse circle5 = new MTEllipse(app, new Vector3D((mtApplication.width/5)*4 + 35, 50), 30, 30);
+			circle5.setFillColor(kleurbol5);
+			circle5.setNoStroke(true);
+			getCanvas().addChild(circle5);
+			final MTTextArea tap5 = new MTTextArea(mtApplication, font);
+			tap5.setFillColor(textAreaColor);
+			tap5.setStrokeColor(textAreaColor);
+			tap5.setText("BUSINESS COMMUNICATION");
+			this.clearAllGestures(tap5);
+			tap5.registerInputProcessor(new TapProcessor(app));
+			tap5.addGestureListener(TapProcessor.class, new IGestureEventListener() {
+				public boolean processGestureEvent(MTGestureEvent ge) {					
+					TapEvent te = (TapEvent)ge;
+					switch (te.getId()) {
+					case MTGestureEvent.GESTURE_DETECTED:
+						MTColor white = new MTColor(10,100,50);
+						break;
+					case MTGestureEvent.GESTURE_UPDATED:
+						break;
+					case MTGestureEvent.GESTURE_ENDED:
+						if (te.isTapped()){
+							app.pushScene();
+							if (scene5 == null){
+								scene5 = new scene5(app, "Scene 5");
+								//Add the scene to the mt application
+								app.addScene(scene5);
+							}
+							//Do the scene change
+							app.changeScene(scene5);
+							break;
+							}
+					default: break;
+				}
+					return false;				
+			}
+			});
+			getCanvas().addChild(tap5);
+			tap5.setAnchor(PositionAnchor.UPPER_LEFT);
+			tap5.setPositionGlobal(new Vector3D((mtApplication.width/5)*4,30,0));
+			
+			//Textarea
+			final MTTextArea tekstinhoud = new MTTextArea(300, 150, 700, 300, inhoudfont, mtApplication); 
+			tekstinhoud.setNoFill(true);
+			tekstinhoud.setNoStroke(true);
+			tekstinhoud.setText("INTEGRATION PROJECT                                                                                                                                          "
+								+"DURING THE FIRST TWO YEARS OF THEIR STUDIES, OUR STUDENTS WILL WORK ON DIFFERENT PROJECTS.      THIS WAY, OUR STUDENTS GET A LOT OF PRATICE BEFORE THEY WILL WORK ON PROJECTS IN A BUSINESS ENVIRONMENT.                                                                                                                                              "
+								+ "PROJECTS:                                                                    "
+								+ "   INTEGRATION WEB: DEVELOPMENT OF A STATIC WEBSITE IN TEAM.                              "
+								+ "   INTEGRATION MOBILE: DEVELOPMENT OF A MOBILE SITE IN TEAM.                               "
+								+ "   INTEGRATION MULTISCREEN: DEVELOPMENT OF AN APPLICATION FOR TOUCHSCREEN IN TEAM.             "
+								+ "   INTEGRATION MOBILE APP AND WEB: HIER MOET NOG TEKST KOMEN !!!                          "
+								+ "   INTEGRATION ART AND TECHNOLOGY: HIER MOET NOG TEKST KOMEN !!!                                                                                                         "
+								+ "INTERNSHIP:                                                                    "
+								+ "   MULTEC PROVIDES THEIR STUDENTS TO WORK IN A REAL BUSNIESS ENVIRONMENT AT THE END OF THEIR STUDIES.                                                                             "
+								+ "FINAL WORK: THIS WILL BE MADE IN THE LAST YEAR.   "
+					);
+			this.clearAllGestures(tekstinhoud);
+			this.getCanvas().addChild(tekstinhoud);
+			
+			//Set a scene transition - Flip transition only available using opengl supporting the FBO extenstion
+			if (MT4jSettings.getInstance().isOpenGlMode() && GLFBO.isSupported(app))
+				this.setTransition(new FadeTransition(app, 700));
+			else{
+				this.setTransition(new FadeTransition(app, 700));
+			}
+								
+		}
+
+		
+		private void clearAllGestures(MTComponent comp){
+			comp.unregisterAllInputProcessors();
+			comp.removeAllGestureEventListeners();
+		}	
+	
+	@Override
+	public void init() {
+
+	}
+
+	@Override
+	public void shutDown() {
+
+	}
+
+}
+
