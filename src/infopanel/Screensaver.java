8c929fe65520c5a398de7ef930e6391d09a1ca2b
wesleyvandevoorde <vandevoordewesley@gmail.com>
2014-01-09 12:20:53 +0000

    Screensaver change positions

--- src/infopanel/Screensaver.java
+++ src/infopanel/Screensaver.java
@@ -34,19 +34,19 @@ public class Screensaver extends AbstractScene {
 		// Create images needed
 		PImage multec = mtApplication.loadImage("multecButton.png");
 		MTRectangle multecRectangle = new MTRectangle(multec, mtApplication);
-		multecRectangle.setPositionGlobal(new Vector3D(425, mtApplication.height/2, 0));
+		multecRectangle.setPositionGlobal(new Vector3D(mtApplication.width/3, (mtApplication.height/2)));
 		
 		PImage digx = mtApplication.loadImage("digx.png");
 		MTRectangle digxRectangle = new MTRectangle(digx, mtApplication);
-		digxRectangle.setPositionGlobal(new Vector3D(mtApplication.width-(mtApplication.width/3)+ (mtApplication.width/35), mtApplication.height/2,0));
+		digxRectangle.setPositionGlobal(new Vector3D((mtApplication.width/3)*2, (mtApplication.height/2)));
 		
 		PImage stripeL = mtApplication.loadImage("stripeL.png");
 		MTRectangle stripeLRectangle = new MTRectangle(stripeL, mtApplication);
-		stripeLRectangle.setPositionGlobal(new Vector3D(75, (mtApplication.height/2)-100));
+		stripeLRectangle.setPositionGlobal(new Vector3D((mtApplication.width/3)-350, (mtApplication.height/2)-100));
 		
 		PImage stripeR = mtApplication.loadImage("stripeR.png");
 		MTRectangle stripeRRectangle = new MTRectangle(stripeR, mtApplication);
-		stripeRRectangle.setPositionGlobal(new Vector3D(mtApplication.width-(mtApplication.width/3)+350 + (mtApplication.width/35), mtApplication.height/2 + 100));
+		stripeRRectangle.setPositionGlobal(new Vector3D(((mtApplication.width/3)*2)+350, (mtApplication.height/2)+100));
 		
 		// put  image to scene
 		
